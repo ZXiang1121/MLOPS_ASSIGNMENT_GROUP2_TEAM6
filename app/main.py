eacf2de572cf30d0d5e6a35dc4527e7a71a5c443
Zhang Xiang <98081173+ZXiang1121@users.noreply.github.com>
2023-08-18 11:29:46 +0800

    Add my css and boostrap design

--- app/main.py
+++ app/main.py
@@ -6,7 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-
+@app.route('/medical_predict')
+def medical():
+    return render_template('medical.html')
 
 
 if __name__ == '__main__':
